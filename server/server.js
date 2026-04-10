918deccfb01a38d8303471baf0aa62ca797eb892
ShubhamMhatre24 <mhatre.y.shubham@gmail.com>
2026-04-10 11:30:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,11 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-  origin: "",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 
 app.use("/api/secondlife/", enquiryrouter);
 app.use('/upload', express.static('upload'));
